ed3da9d3eb65131e032f08ec8cb826186654d0b8
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-09-29 14:31:05 +0700

    feat: create lodash inRange functionality

--- Back-End Engineer - Career Path/4 - JavaScript Syntax - Part II/Lodash_Project/_.js
+++ Back-End Engineer - Career Path/4 - JavaScript Syntax - Part II/Lodash_Project/_.js
@@ -4,6 +4,21 @@ const _ = {
     const clampedValue = Math.min(lowerClampedValue, upper);
     return clampedValue;
   },
+  inRange(number, start, end) {
+    if (end === undefined) {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+
+    let isInRange = start <= number && number < end;
+
+    return isInRange;
+  },
 };
 
 // Do not write or modify code below this line.
